6765c7edb2b0f1e62b915296a138bab9d5022070
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-25 17:05:12 +0530

    Improve contact card ui

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -11,19 +11,74 @@ interface ContactCardProps {
 
 const ContactCard: React.FC<ContactCardProps> = ({ contactDetails }) => {
   const dispatch = useDispatch();
-  const {id,firstname, lastname, status } = contactDetails;
+  const { id, firstname, lastname, status } = contactDetails;
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-6">
-      <h2 className="text-xl font-semibold">{firstname}</h2>
-      <p className="text-gray-600 mb-2 truncate">{lastname}</p>
-      <p className="text-gray-500 capitalize">{status}</p>
-      <div className="mt-10 flex flex-col gap-5">
-        <Link
-          to={`/edit-contact/${id}`}
-          className="inline-flex px-4 py-3 bg-green-600 hover:bg-green-500  justify-center text-white items-center rounded-md"
-        >
-          <button type="button" className="flex font-semibold">
+    <div className="bg-white rounded-md border">
+      <div className="p-4">
+        <h1 className="inline-flex items-center text-2xl font-semibold capitalize">
+          {firstname} {lastname}
+        </h1>
+        <div className="flex gap-2 text-xl text-gray-600 items-center my-4">
+          {status === 'active' ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#19c17c"
+              className="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#FF6666"
+              className="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          )}
+
+          {status}
+        </div>
+
+        <div className="mt-10 flex flex-col gap-5">
+          <Link
+            to={`/edit-contact/${id}`}
+            className="inline-flex  py-2 bg-green-600 hover:bg-green-500  justify-center text-white items-center rounded-md"
+          >
+            <button type="button" className="flex font-semibold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 mr-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+              Edit
+            </button>
+          </Link>
+          <button
+            type="button"
+            className="inline-flex justify-center items-center rounded-md bg-red-600 py-2 text-sm font-semibold text-white hover:bg-red-500"
+            onClick={() => dispatch(deleteContact(contactDetails))}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -35,33 +90,12 @@ const ContactCard: React.FC<ContactCardProps> = ({ contactDetails }) => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
               />
             </svg>
-            Edit
+            Delete
           </button>
-        </Link>
-        <button
-          type="button"
-          className="inline-flex justify-center items-center rounded-md bg-red-600 px-4 py-3 text-sm font-semibold text-white hover:bg-red-500"
-          onClick={() => dispatch(deleteContact(contactDetails))}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 mr-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            />
-          </svg>
-          Delete
-        </button>
+        </div>
       </div>
     </div>
   );
